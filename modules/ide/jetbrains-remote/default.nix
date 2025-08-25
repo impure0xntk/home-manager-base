ae86a2e9920c5127fd8bd01b81d2a776f6a9d0ab
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-08-25 23:47:45 +0900

    feat(jetbrains-remote): add auto patch for remote-dev-server

--- modules/ide/jetbrains-remote/default.nix
+++ modules/ide/jetbrains-remote/default.nix
@@ -15,5 +15,19 @@ in {
       enable = true;
       ides = cfg.ides;
     };
+    systemd.user.services.jetbrains-remote-dev-watcher = {
+      Unit = {
+        Description = "Watch JetBrains RemoteDev directory for changes";
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.inotify-tools}/bin/inotifywait -m -r -e create,modify ~/.cache/JetBrains/RemoteDev/dist/ --format '%w%f' | while read file; do
+          if echo \"$file\" | grep -q 'bin/.*\.sh$'; then
+            ${pkgs.patchelf}/bin/patchelf --set-rpath \"${lib.makeLibraryPath [pkgs.zlib]}\" \"$file\"
+          fi
+        done";
+      };
+      Install.WantedBy = [ "default.target" ];
+    };
   };
 }
