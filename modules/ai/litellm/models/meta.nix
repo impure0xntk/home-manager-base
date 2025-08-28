3dbd2c83b7264efee5da00e10c9a12bf97eb9290
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-08-28 23:00:24 +0900

    feat(ai/models): llama-4-{scout,maverick}

--- modules/ai/litellm/models/meta.nix
+++ modules/ai/litellm/models/meta.nix
@@ -1,4 +1,11 @@
 { lib, ... }:
+let
+  llm-4-parameters = {
+    temperature = 0.6;
+    top_p = 0.9;
+    min_p = 0.01;
+  };
+in
 
 {
   "llama-3.3" = [
@@ -7,6 +14,22 @@
       params = {
         weight = 10;
       };
+    }
+  ];
+  llama-4-maverick = [
+    {
+      model = "openrouter/meta-llama/llama-4-maverick:free";
+      params = {
+        weight = 10;
+      } // llm-4-parameters;
+    }
+  ];
+  llama-4-scout = [
+    {
+      model = "openrouter/meta-llama/llama-4-scout:free";
+      params = {
+        weight = 10;
+      } // llm-4-parameters;
     }
   ];
 }
