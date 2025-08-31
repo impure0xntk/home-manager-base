623ef1a69f4eb4cc94e619258b2f9581ef84114b
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-08-31 23:25:57 +0900

    refactor(jetbrains-remote): improve FHS wrapper and activation setup

--- modules/ide/jetbrains-remote/default.nix
+++ modules/ide/jetbrains-remote/default.nix
@@ -9,19 +9,54 @@ let
 
   # Attention
   # In 2025-08-30, NixOS can no longer launch Jetbrains ide without buildFHSUserEnv.
-  idesFHSWrapped = (
-    map (
-      ide:
-      pkgs.buildFHSUserEnv rec {
-        name = ide.meta.mainProgram or ide.pname;
-        targetPkgs = pkgs: [ ide ];
-        runScript = name;
-      }
-    ) cfg.ides
-  );
-in
-{
+  #
+  # For jetbrains-remote only because of specific mainProgram setting.
+  ideFHSWrapped = ide: name:
+    let
+      unwrapped = ide;
+    in
+    (pkgs.buildFHSEnv {
+      # name = ide.meta.mainProgram or ide.pname;
+      inherit name;
+      inherit (unwrapped) version;
+      targetPkgs = pkgs: [ unwrapped ];
+      runScript = name;
+      passthru = {
+        inherit unwrapped;
+      };
+    });
 
+  # idesFHSWrapped = map (ide: ideFHSWrapped ide (ide.meta.mainProgram or ide.pname)) cfg.ides;
+  idesFHSWrapped = map ideWrapped cfg.ides;
+  ideWrapped = ide:
+    let
+      mainProgram = ide.meta.mainProgram or ide.pname;
+      mainWrapped = ideFHSWrapped ide mainProgram;
+      remoteDevServerWrapped = ideFHSWrapped ide "${mainProgram}-remote-dev-server";
+    in ide.overrideAttrs (final: prev: {
+    nativeBuildInputs = (prev.nativeBuildInputs or []) ++ [
+      pkgs.coreutils
+    ];
+
+    postInstall = (prev.postInstall or "") + ''
+      # makeWrapper "${lib.getExe mainWrapped}" "$out/bin/${final.meta.mainProgram}"
+
+      MAIN_PROGRAM_PATH="$(readlink -f $out/bin/${final.meta.mainProgram})"
+      REMOTE_DEV_SERVER_PATH="$(readlink -f $out/bin/${final.meta.mainProgram}-remote-dev-server)"
+
+      mv "$MAIN_PROGRAM_PATH"{,--unwrapped}
+      mv "$REMOTE_DEV_SERVER_PATH"{,--unwrapped}
+      mv "$out/${final.pname}/bin/remote-dev-server"{,--unwrapped} || true
+      unlink $out/bin/${final.meta.mainProgram}
+      unlink $out/bin/${final.meta.mainProgram}-remote-dev-server
+      ln -s "${lib.getExe mainWrapped}" "$out/bin/${final.meta.mainProgram}"
+      ln -s "${lib.getExe mainWrapped}" "$MAIN_PROGRAM_PATH"
+      ln -s "${lib.getExe remoteDevServerWrapped}" "$out/bin/${final.meta.mainProgram}-remote-dev-server"
+      ln -s "${lib.getExe remoteDevServerWrapped}" "$REMOTE_DEV_SERVER_PATH"
+      ln -s "${lib.getExe remoteDevServerWrapped}" "$out/${final.pname}/bin/remote-dev-server"
+    '';
+  });
+in {
   options.my.home.ide.jetbrains-remote = {
     enable = lib.mkEnableOption "Whether to enable JetBrains Remote Development.";
     ides = lib.mkOption {
@@ -34,5 +69,23 @@ in
       enable = true;
       ides = idesFHSWrapped;
     };
+    home.activation.jetBrainsRemote =
+      let
+        distPath = "${config.xdg.cacheHome}/JetBrains/RemoteDev/userProvidedDist";
+        mkLine =
+          ide:
+          ''
+            ${ide}/bin/${ide.meta.mainProgram}-remote-dev-server registerBackendLocationForGateway || true;
+            unlink ${distPath}/${lib.replaceStrings ["-"] ["_"] ide.pname} || true
+            ln -s ${ide}/${ide.pname} ${distPath}/${lib.replaceStrings ["-"] ["_"] ide.pname}
+          '';
+        lines = map mkLine config.programs.jetbrains-remote.ides;
+        linesStr =
+          ''
+            rm ${distPath}/_nix_store* || true
+          ''
+          + lib.concatStringsSep "\n" lines;
+      in
+      lib.mkForce (lib.hm.dag.entryAfter [ "writeBoundary" ] linesStr);
   };
 }
